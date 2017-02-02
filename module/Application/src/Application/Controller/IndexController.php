a0cce3b382714a33a271c375a67720990088e8b9
tomoakinishimura <n.t0226@gmail.com>
2017-02-03 00:43:51 +0900

    google custom api取得ロジック作成

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -11,13 +11,43 @@ namespace Application\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use Application\Form\IndexForm;
 
 class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
-       
+        $request = $this->getRequest();
+        //var_dump('hoge');exit;
+        if ($request->isPost()) {
+            
+            $post_parameter = $request->getPost('searchText');
+            
+            $result = $this->getGoogleSearchResult($post_parameter);
+            echo $result;
+        }
         
-        return new ViewModel();
+        
+        //form return
+        $title = 'Google Console Search';
+        
+        return new ViewModel(array(
+            'title'  => $title,
+        ));
     }
+    
+    private function getGoogleSearchResult($post_parameter)
+    {
+        $target_str = $post_parameter;
+        $url = 'http://search.yahoo.co.jp/search?p='.urlencode($target_str);
+        $data = mb_convert_encoding(file_get_contents($url), "utf8", "auto");
+        
+        return $data;
+    }
+
+        private function getGoogleService()
+    {
+        return;
+    }
+    
 }
---
+++ module/Application/src/Application/Model/Google.php
@@ -0,0 +1,85 @@
+<?php
+namespace Application\Model;
+ 
+// 4つインポート
+use Zend\InputFilter\Factory as InputFactory;
+use Zend\InputFilter\InputFilter;
+use Zend\InputFilter\InputFilterAwareInterface;
+use Zend\InputFilter\InputFilterInterface;
+ 
+class Google
+{
+    public $id;
+    public $artist;
+    public $title;
+    protected $inputFilter; // <- これを追加
+ 
+    // この辺から追加
+    public function setInputFilter(InputFilterInterface $inputFilter)
+    {
+        throw new \Exception("Not used");
+    }
+ 
+    public function getInputFilter()
+    {
+        if (!$this->inputFilter) {
+            $inputFilter = new InputFilter();
+            $factory     = new InputFactory();
+ 
+            $inputFilter->add($factory->createInput(array(
+                'name'     => 'id',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'Int'),
+                ),
+            )));
+ 
+            $inputFilter->add($factory->createInput(array(
+                'name'     => 'artist',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'StripTags'),
+                    array('name' => 'StringTrim'),
+                ),
+                'validators' => array(
+                    array(
+                        'name'    => 'StringLength',
+                        'options' => array(
+                            'encoding' => 'UTF-8',
+                            'min'      => 1,
+                            'max'      => 100,
+                        ),
+                    ),
+                ),
+            )));
+ 
+            $inputFilter->add($factory->createInput(array(
+                'name'     => 'title',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'StripTags'),
+                    array('name' => 'StringTrim'),
+                ),
+                'validators' => array(
+                    array(
+                        'name'    => 'StringLength',
+                        'options' => array(
+                            'encoding' => 'UTF-8',
+                            'min'      => 1,
+                            'max'      => 100,
+                        ),
+                    ),
+                ),
+            )));
+ 
+            $this->inputFilter = $inputFilter;
+        }
+ 
+        return $this->inputFilter;
+    }
+ 
+    public function exchangeArray($data)
+    {
+       // 略
+    }
+}
